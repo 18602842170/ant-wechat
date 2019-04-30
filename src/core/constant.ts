6431e2a87fc3c4fa85305972d3cf3f1afee1afe9
18602842170 <31336314+18602842170@users.noreply.github.com>
2019-04-30 14:57:07 +0800

    Update constant.ts

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,7 +1,4 @@
-export const BACKEND_URL = "https://www.kaimadata.com.cn";
-
-// export const BACKEND_URL = "http://127.0.0.1:4200";
-// export const BACKEND_URL = "http://139.224.236.68:4200";
+export const BACKEND_URL = "http://127.0.0.1:4200";
 // export const BACKEND_URL = "http://114.116.28.17:4200";
 
 export const LOGIN_URL = "/pages/login/login";
@@ -9,3 +6,5 @@ export const LOGIN_URL = "/pages/login/login";
 export const LOGIN_DEFAULT_URL = "/pages/dashboar/dashboar";
 
 export const TOKEN_NAME = "tsms";
+
+// 此文件中为后台访问路径。请自行修改
